aaad54bd3e5e5416cccca107cb7adb4cb3ea6815
mjhwest89 <mjhwest89@gmail.com>
2021-11-23 16:25:29 +1030

    initial commit

---
+++ server.js
@@ -0,0 +1,14 @@
+// import required modules and packages 
+const inquirer = require('inquirer');
+
+
+const mysql = require('mysql2');
+//https://www.npmjs.com/package/mysql2  <<<<<<<< documentation 
+
+
+const table = require('console.table');
+//https://www.npmjs.com/package/console.table <<<<<<<documentation here
+
+
+const logo = require('asciiart-logo');
+// https://www.npmjs.com/package/asciiart-logo/v/0.2.5 <<<<<<<<<<documentation here
