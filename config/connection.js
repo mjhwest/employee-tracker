58021102081c59e64dfb950638d38bc6fd73141c
mjhwest89 <mjhwest89@gmail.com>
2021-11-25 00:49:10 +1030

    updated server.js

--- config/connection.js
+++ config/connection.js
@@ -1,14 +1,13 @@
-const Sequelize = require('sequelize');
 require('dotenv').config();
+const mysql = require("mysql")
 
-const sequelize = new Sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USER,
-    process.env.DB_PASSWORD, {
-        host: process.env.DB_HOST,
-        dialect: 'mysql',
-        port: 3306
-    }
-);
+function connection() {
+    return mysql.createConnection({
+        host: 'localhost',
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_NAME
+    });
+}
 
-module.exports = sequelize;
+module.exports = connection;
--- server.js
+++ server.js
@@ -1,23 +1,129 @@
-// const inquirer = require('inquirer');
-// const mysql = require('mysql2');
-// //https://www.npmjs.com/package/mysql2  <<<<<<<< documentation 
+const inquirer = require('inquirer');
+const mysql = require('mysql2');
+//https://www.npmjs.com/package/mysql2  <<<<<<<< documentation 
+
+const table = require('console.table');
+//https://www.npmjs.com/package/console.table <<<<<<<documentation here
 
 
-// const table = require('console.table');
-// //https://www.npmjs.com/package/console.table <<<<<<<documentation here
+require('dotenv').config();
+//use dotenv so that credentials can be hidden
+
+//code for figlet for special graphic at stat of application.
+var figlet = require('figlet');
+const connection = require('./config/connection');
+figlet('Employee Tracker', function(err, data) {
+    if (err) {
+        console.log('Something went wrong...');
+        console.dir(err);
+        return;
+    }
+    console.log(data)
+});
 
 
-// const logo = require('asciiart-logo');
-// // https://www.npmjs.com/package/asciiart-logo/v/0.2.5 <<<<<<<<<<documentation
+//create connecting ti SQL database. 
 
-// require('dotenv').config();
-// //use dotenv so that credentials can be hidden
+const db = mysql.createConnection({
+        host: 'localhost',
+        port: 3306,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_NAME
+    },
+    console.log('Connected to the employees_db database.')
+);
+
+//connect to server and DB
+// connection.connect(function(err) {
+//     if (err) throw err;
+//     employeeTracker();
+// });
+
+
+//readme for homework says "use a seprare file that contains functions for perfroming specigic SQL queries"
+//need to create a folder than contains function for
+//1) View All department, 2) View All Roles, 3) View All employees, 4) Add a department, 5) Add a role
+//6) Add an employee, 7) Update employee role. 
+
+
+
+
+const menu = () => {
+    inquirer.prompt([{
+        type: "list",
+        message: "Welcome to the employee trakcer. Here you can select the options to view your work strucure and make changes, ",
+        name: "Menu",
+        choices: [
+            "View All Departments",
+            "View All Roles",
+            "View All Employees",
+            "Add A Department",
+            "Add A Role",
+            "Add An Employee",
+            "Update An Emplyoee Role",
+            "Exit Applicatoin"
+        ]
+    }])
+
+    .then((answer) => {
+        switch (answer.menu) {
+            case "View All Departments":
+                viewAllDeparments();
+
+                break;
+            case "View All Roles":
+                viewAllRoles();
+
+                break;
+            case "View All Employees":
+                viewAllEmployees();
+
+                break;
+            case "Add A Department":
+                addADepartment();
+
+                break;
+            case "Add A Role":
+                addARole();
+
+                break;
+            case "Add An Employee":
+                addAnEmployee();
+
+                break;
+            case "Update An Employee Role":
+                updateAnEmployeeRole();
+
+                break;
+            case "Exit Application":
+                console.log('Exiting the app ...\n');
+                connection.end();
+                break;
+            default:
+                console.log(`Invalid Selection: ${answer.menu}`);
+                break;
+        }
+    })
+
+    menu()
+};
+
+
+// query the database, get function to view all departments. 
+function viewAllDeparments() {
+    db.query('SELECT * FROM department', function(err, results) {
+        if (err) throw err;
+        console.log(results);
+        menu();
+    });
+
+}
 
 
 
 
 
-// //MAIN MENU 
-// const databaseMenu = () => {
 
-// }
+
+module.exports = menu;
