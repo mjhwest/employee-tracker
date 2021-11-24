d5db09c4913317fcb2671601bcf23eed93fdbda5
mjhwest89 <mjhwest89@gmail.com>
2021-11-24 14:38:06 +1030

    added first inquirer for inital question

--- server.js
+++ server.js
@@ -1,17 +1,23 @@
-// import required modules and packages 
-const inquirer = require('inquirer');
+// const inquirer = require('inquirer');
+// const mysql = require('mysql2');
+// //https://www.npmjs.com/package/mysql2  <<<<<<<< documentation 
 
 
-const mysql = require('mysql2');
-//https://www.npmjs.com/package/mysql2  <<<<<<<< documentation 
+// const table = require('console.table');
+// //https://www.npmjs.com/package/console.table <<<<<<<documentation here
 
 
-const table = require('console.table');
-//https://www.npmjs.com/package/console.table <<<<<<<documentation here
+// const logo = require('asciiart-logo');
+// // https://www.npmjs.com/package/asciiart-logo/v/0.2.5 <<<<<<<<<<documentation
+
+// require('dotenv').config();
+// //use dotenv so that credentials can be hidden
 
 
-const logo = require('asciiart-logo');
-// https://www.npmjs.com/package/asciiart-logo/v/0.2.5 <<<<<<<<<<documentation
 
-require('dotenv').config();
-//use dotenv so that credentials can be hidden
+
+
+// //MAIN MENU 
+// const databaseMenu = () => {
+
+// }
